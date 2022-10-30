16c007f0e205c42468b0897a667635d793f86028
volodymyr-ok <109730758+volodymyr-ok@users.noreply.github.com>
2022-10-30 11:48:59 +0000

    Deploying to gh-pages from @ volodymyr-ok/goit-js-project-cocktails@3e9988c7067b30f9abbb2e079fb1c52f23e00021 🚀

---
+++ index.ca18562f.js
@@ -0,0 +1,2 @@
+const o=document.querySelectorAll(".btn-like"),e=document.querySelectorAll(".btn-read-more"),t=document.querySelector(".cocktails");console.log(t),console.log(o),t.addEventListener("click",(function(t){if(console.log(t),console.log(t.target.textContent),console.log(t.path),t.target===t.currentTarget)return void console.log("abc");t.target===o&&console.log("uhaaa");t.target===e&&console.log("paramparampam");t.path[0]===o&&console.log("uhaaaa")}));
+//# sourceMappingURL=index.ca18562f.js.map
