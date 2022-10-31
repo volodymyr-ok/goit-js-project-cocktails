33c38c6008ddf0d345c9211894ec3c8c004c1384
Volodymyr Ok <v.shark.t@gmail.com>
2022-10-31 20:49:55 +0200

    made refactoring in js and merged random-cocktails.js to pagination.js

--- src/js/random-cocktails.js
+++ src/js/random-cocktails.js
@@ -1,66 +1,68 @@
-import debounce from 'lodash.debounce';
+// import debounce from 'lodash.debounce';
+// const cocktailList = document.querySelector('.cocktails__list');
+// const srchInput = document.querySelector('[name="cocktail-search"]');
+// const abcSelect = document.querySelector('#abc-cocktails');
+// let cocktailsPerPage = 3;
 
-const cocktailList = document.querySelector('.cocktails__list');
-const srchInput = document.querySelector('[name="cocktail-search"]');
-const abcSelect = document.querySelector('#abc-cocktails');
+// getRandomCocktails();
 
-let cocktailsPerPage = 3;
+// window.addEventListener('resize', debounce(resizeListener, 500));
+// function resizeListener() {
+//   if (srchInput.value === '' && abcSelect.value === '') {
+//     getRandomCocktails();
+//   }
+// }
 
-getRandomCocktails();
+// async function getRandomCocktails() {
+//   console.log('* Start random with innerWidth *', innerWidth);
+//   try {
+//     if (innerWidth < 768) {
+//       cocktailsPerPage = 3;
+//     } else if (innerWidth >= 768 && innerWidth < 1280) {
+//       cocktailsPerPage = 6;
+//     } else if (innerWidth >= 1280) {
+//       cocktailsPerPage = 9;
+//     }
 
-window.addEventListener('resize', debounce(resizeListener, 500));
-function resizeListener() {
-  if (srchInput.value === '' && abcSelect.value === '') {
-    getRandomCocktails();
-  }
-}
+//     const randomCocktailsArray = [];
 
-async function getRandomCocktails() {
-  console.log('* Start random with innerWidth *', innerWidth);
-  try {
-    if (innerWidth < 768) {
-      cocktailsPerPage = 3;
-    } else if (innerWidth >= 768 && innerWidth < 1280) {
-      cocktailsPerPage = 6;
-    } else if (innerWidth >= 1280) {
-      cocktailsPerPage = 9;
-    }
+//     for (let i = 0; i < cocktailsPerPage; i++) {
+//       const response = await fetchRandomCocktail();
+//       randomCocktailsArray.push(...response.drinks);
+//     }
 
-    const randomCocktailsArray = [];
+//     cocktailList.innerHTML = randomCocktailsArray.map(updateMarkup).join('');
+//   } catch (error) {
+//     throw new Error('Помилка у getRandomCocktails', error.message);
+//   }
+// }
 
-    for (let i = 0; i < cocktailsPerPage; i++) {
-      const response = await fetchRandomCocktail();
-      randomCocktailsArray.push(...response.drinks);
-    }
+// async function fetchRandomCocktail() {
+//   try {
+//     const randomCocktail = await fetch(
+//       'https://www.thecocktaildb.com/api/json/v1/1/random.php'
+//     );
+//     return randomCocktail.json();
+//   } catch (error) {
+//     throw new Error('Помилка у fetchRandomCocktail', error.message);
+//   }
+// }
 
-    cocktailList.innerHTML = randomCocktailsArray.map(updateMarkup).join('');
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-async function fetchRandomCocktail() {
-  const randomCocktail = await fetch(
-    'https://www.thecocktaildb.com/api/json/v1/1/random.php'
-  );
-  return randomCocktail.json();
-}
-
-const updateMarkup = ({ strDrink, strDrinkThumb, idDrink }) => `
-  <li class="coctails__item">
-    <img src="${strDrinkThumb}" alt="${strDrink}" width="280" height="280" />
-    <h2 class="cocktails__item-title">${strDrink}</h2>
-    <div class="coctails__btns">
-      <button class="btn__read-more" type="button" id="${idDrink}">
-        Learn more
-      </button>
-      <button id='${idDrink}' class="btn__like" type="button">
-        Add to
-        <svg class="btn__like-icon"><use xlink:href="#icon-Heart"><symbol id="icon-Heart" viewBox="0 0 35 32">
-        <path fill="#fd5103" style="fill: var(--color1, #fd5103)" d="M17.684 32l-2.564-2.302c-9.107-8.144-15.12-13.515-15.12-20.107 0-5.371 4.28-9.591 9.726-9.591 3.077 0 6.030 1.413 7.958 3.645 1.928-2.232 4.881-3.645 7.958-3.645 5.447 0 9.726 4.22 9.726 9.591 0 6.592-6.013 11.963-15.12 20.124l-2.564 2.284z"></path>
-        <path  d="M17.684 28.632l-2.076-1.817c-7.373-6.429-12.24-10.67-12.24-15.874 0-4.24 3.464-7.572 7.874-7.572 2.491 0 4.882 1.115 6.442 2.877 1.56-1.762 3.951-2.877 6.442-2.877 4.409 0 7.874 3.332 7.874 7.572 0 5.204-4.867 9.444-12.24 15.888l-2.076 1.803z"></path>
-        </symbol></use></svg>
-      </button>
-    </div>
-  </li>
-`;
+// const updateMarkup = ({ strDrink, strDrinkThumb, idDrink }) => `
+//   <li class="coctails__item">
+//     <img src="${strDrinkThumb}" alt="${strDrink}" width="280" height="280" />
+//     <h2 class="cocktails__item-title">${strDrink}</h2>
+//     <div class="coctails__btns">
+//       <button class="btn__read-more" type="button" id="${idDrink}">
+//         Learn more
+//       </button>
+//       <button id='${idDrink}' class="btn__like" type="button">
+//         Add to
+//         <svg class="btn__like-icon"><use xlink:href="#icon-Heart"><symbol id="icon-Heart" viewBox="0 0 35 32">
+//         <path fill="#fd5103" style="fill: var(--color1, #fd5103)" d="M17.684 32l-2.564-2.302c-9.107-8.144-15.12-13.515-15.12-20.107 0-5.371 4.28-9.591 9.726-9.591 3.077 0 6.030 1.413 7.958 3.645 1.928-2.232 4.881-3.645 7.958-3.645 5.447 0 9.726 4.22 9.726 9.591 0 6.592-6.013 11.963-15.12 20.124l-2.564 2.284z"></path>
+//         <path  d="M17.684 28.632l-2.076-1.817c-7.373-6.429-12.24-10.67-12.24-15.874 0-4.24 3.464-7.572 7.874-7.572 2.491 0 4.882 1.115 6.442 2.877 1.56-1.762 3.951-2.877 6.442-2.877 4.409 0 7.874 3.332 7.874 7.572 0 5.204-4.867 9.444-12.24 15.888l-2.076 1.803z"></path>
+//         </symbol></use></svg>
+//       </button>
+//     </div>
+//   </li>
+// `;
