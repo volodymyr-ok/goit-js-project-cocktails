90bd0f52a5f096bc6a31ea0d6102c318519dff26
Yurii Vynar <yurik.vinar37@gmail.com>
2022-11-01 22:44:57 +0200

    modal

---
+++ src/js/theme.js
@@ -0,0 +1,31 @@
+const input = document.querySelector('.checkbox-input')
+
+
+console.log(input.checked=true)
+document.querySelector('.checkbox-input').addEventListener('click', (event)=>{
+    if(localStorage.getItem('theme')==='dark'){
+        localStorage.removeItem('theme')
+        console.log(input.checked)
+    }else{
+        localStorage.setItem('theme', 'dark')
+        console.log(input.checked)
+    }
+     addDarkClassToHtml()
+})
+
+function addDarkClassToHtml(){
+    try{
+    if(localStorage.getItem('theme')==='dark'){
+        document.querySelector('html').classList.add('dark')
+        input.checked=true
+    }else{
+        document.querySelector('html').classList.remove('dark')
+        input.checked=false
+    }
+
+ }catch(error){
+
+ }
+}
+ addDarkClassToHtml()
+
--- src/js/yuras.js
+++ src/js/yuras.js
@@ -6,7 +6,7 @@ setTimeout(() => {
     reload()
 
     const btnLike = document.querySelectorAll('.btn__like')
-console.log(btnLike)
+// console.log(btnLike)
 btnLike.forEach(el=>el.addEventListener('click', userChoise))
 function userChoise(event){
     if(event.target.id.length===0){
@@ -15,12 +15,14 @@ function userChoise(event){
     if(!storage.includes(event.target.id)){
         storage.push(event.target.id) 
         event.path[0].lastElementChild.classList.add("active-like-btn")
+       event.path[0].firstChild.textContent='Remove'
     }else{
         storage.splice(storage.indexOf(event.target.id), 1)
         event.path[0].lastElementChild.classList.remove("active-like-btn")
+        event.path[0].firstChild.textContent='Add to'
     }
     localStorage.setItem('drinksId', storage)
-    console.log(storage)
+    // console.log(storage)
 }
   }, 5000)
   
@@ -28,10 +30,11 @@ function userChoise(event){
   function reload () {
    let user = localStorage.getItem('drinksId')
     if (user){
-    console.log(user.split(','))
+    // console.log(user.split(','))
     let ara = user.split(',')
     ara.forEach(el=>storage.push(el))
 
     }
 
 }
+
