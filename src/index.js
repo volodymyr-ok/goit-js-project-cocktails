a6fad477aebe46822a59e30d239be6c40ba34186
Yurii Vynar <yurik.vinar37@gmail.com>
2022-11-02 18:27:18 +0200

    qwertyu

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-// import './js/mob.menu.js';
-import './js/random-cocktails.js';
-import './js/local-storage.js';
-// import './js/pagination.js';
+import './js/mob.menu.js';
+import './js/pagination.js';
 import './js/yuras.js';
 import './js/yuras-m.js';
-// import './js/theme.js';
-import './js/local-coc'
+import './js/theme.js';
+// import './js/cocktails-storage';
+// import './js/ingr-storage.js';
--- src/js/yuras.js
+++ src/js/yuras.js
@@ -1,10 +1,9 @@
 export const storage = [];
-// let
+
 export function actionOnLikeBtn() {
   reload();
 
   const btnLike = document.querySelectorAll('.btn__like');
-  // console.log(btnLike)
   btnLike.forEach(el => el.addEventListener('click', userChoise));
   function userChoise(event) {
     if (event.target.id.length === 0) {
@@ -20,39 +19,12 @@ export function actionOnLikeBtn() {
       event.path[0].firstChild.textContent = 'Add to';
     }
     localStorage.setItem('drinksId', storage);
-    // console.log(storage)
   }
 }
-
-// setTimeout(() => {
-
-//     reload()
-
-//     const btnLike = document.querySelectorAll('.btn__like')
-// // console.log(btnLike)
-// btnLike.forEach(el=>el.addEventListener('click', userChoise))
-// function userChoise(event){
-//     if(event.target.id.length===0){
-//         return
-//     }
-//     if(!storage.includes(event.target.id)){
-//         storage.push(event.target.id)
-//         event.path[0].lastElementChild.classList.add("active-like-btn")
-//        event.path[0].firstChild.textContent='Remove'
-//     }else{
-//         storage.splice(storage.indexOf(event.target.id), 1)
-//         event.path[0].lastElementChild.classList.remove("active-like-btn")
-//         event.path[0].firstChild.textContent='Add to'
-//     }
-//     localStorage.setItem('drinksId', storage)
-//     // console.log(storage)
-// }
-//   }, 5000)
 
 function reload() {
   let user = localStorage.getItem('drinksId');
   if (user) {
-    // console.log(user.split(','))
     let ara = user.split(',');
     ara.forEach(el => storage.push(el));
   }
