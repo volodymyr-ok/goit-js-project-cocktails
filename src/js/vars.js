c8b43ddcf6823bff385dc041c552bea7ca466215
Volodymyr Ok <v.shark.t@gmail.com>
2022-11-03 14:32:09 +0200

    forgotten js before refactoring commit

---
+++ src/js/vars.js
@@ -0,0 +1,22 @@
+export const refs = {
+  cocktailList: document.querySelector('.cocktails__list'),
+  srchInput: document.querySelector('.input-tablet'),
+  mobInput: document.querySelector('.input-mobile'),
+  abcSelect: document.querySelector('#letters'),
+  abcList: document.querySelector('.letters-list'),
+  randomCocktailURL: 'https://www.thecocktaildb.com/api/json/v1/1/random.php',
+  searchByFilterURL: 'https://www.thecocktaildb.com/api/json/v1/1/search.php?',
+  searchByFilterURL: 'https://www.thecocktaildb.com/api/json/v1/1/search.php?',
+  htmlNumBar: document.querySelectorAll('.navigation__item'),
+  title: document.querySelector('.cocktails__title'),
+};
+
+// const cocktailList = document.querySelector('.cocktails__list');
+// const srchInput = document.querySelector('.input-tablet');
+// const mobInput = document.querySelector('.input-mobile');
+// const abcSelect = document.querySelector('#letters');
+// const abcList = document.querySelector('.letters-list');
+// const randomCocktailURL =
+//   'https://www.thecocktaildb.com/api/json/v1/1/random.php';
+// const searchByFilterURL =
+//   'https://www.thecocktaildb.com/api/json/v1/1/search.php?';
