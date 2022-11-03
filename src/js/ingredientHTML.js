6eb5f230a911e5d5718b7d64ebc29ed092491ad7
Yurii Vynar <yurik.vinar37@gmail.com>
2022-11-04 01:13:19 +0200

    qwertyuio

--- src/js/ingredientHTML.js
+++ src/js/ingredientHTML.js
@@ -2,6 +2,47 @@ import { storageIngr, markupModalIngr, srchByIngr, modalIngrInteraction } from "
 
 import { checkLocalStorageIngr } from "./check-local-storageIngr";
 
+
+
+const openBtn = document.querySelector('.menu-open');
+const menu = document.querySelector('.menu-burger');
+const closeBtn = document.querySelector('.menu-close');
+const favoriteMenuOpen = document.querySelector('.favorites');
+const favoriteMenuDesk = document.querySelector('.favorite');
+const favoritePages = document.querySelector('.favorite-pages');
+const favoriteDesktop = document.querySelector('.favorite-pages-desk');
+const arrowIcon = document.querySelector('.arrow-icon');
+
+openBtn.addEventListener('click', () => {
+  menu.classList.remove('is-hidden');
+  console.log('click');
+});
+closeBtn.addEventListener('click', () => {
+  menu.classList.add('is-hidden');
+});
+
+favoriteMenuOpen.addEventListener('mouseover', () => {
+  favoritePages.classList.remove('is-hidden');
+  arrowIcon.style.transform = 'rotate(180deg)';
+});
+favoriteMenuOpen.addEventListener('mouseout', () => {
+  setTimeout(() => {
+    favoritePages.classList.add('is-hidden');
+    arrowIcon.style.transform = 'rotate(0)';
+  }, 5000);
+});
+
+favoriteMenuDesk.addEventListener('mouseover', () => {
+  favoriteDesktop.classList.remove('is-hidden');
+});
+
+favoriteMenuDesk.addEventListener('mouseout', () => {
+  setTimeout(() => {
+    favoriteDesktop.classList.add('is-hidden');
+  }, 3000);
+});
+
+
 const ingrList = document.querySelector('.favorite-ingridients');
 const emptyText = document.querySelector('.empty-text');
 console.log(ingrList)
--- src/js/local-coc.js
+++ src/js/local-coc.js
@@ -1,9 +1,51 @@
 
+
+
 import { storage } from "./yuras"
 console.log(storage)
 import { actionOnLikeBtn } from "./yuras"
 import { reload } from "./yuras";
 import { showModalInfo } from './modal-coctail'
+
+
+const openBtn = document.querySelector('.menu-open');
+const menu = document.querySelector('.menu-burger');
+const closeBtn = document.querySelector('.menu-close');
+const favoriteMenuOpen = document.querySelector('.favorites');
+const favoriteMenuDesk = document.querySelector('.favorite');
+const favoritePages = document.querySelector('.favorite-pages');
+const favoriteDesktop = document.querySelector('.favorite-pages-desk');
+const arrowIcon = document.querySelector('.arrow-icon');
+
+openBtn.addEventListener('click', () => {
+  menu.classList.remove('is-hidden');
+  console.log('click');
+});
+closeBtn.addEventListener('click', () => {
+  menu.classList.add('is-hidden');
+});
+
+favoriteMenuOpen.addEventListener('mouseover', () => {
+  favoritePages.classList.remove('is-hidden');
+  arrowIcon.style.transform = 'rotate(180deg)';
+});
+favoriteMenuOpen.addEventListener('mouseout', () => {
+  setTimeout(() => {
+    favoritePages.classList.add('is-hidden');
+    arrowIcon.style.transform = 'rotate(0)';
+  }, 5000);
+});
+
+favoriteMenuDesk.addEventListener('mouseover', () => {
+  favoriteDesktop.classList.remove('is-hidden');
+});
+
+favoriteMenuDesk.addEventListener('mouseout', () => {
+  setTimeout(() => {
+    favoriteDesktop.classList.add('is-hidden');
+  }, 3000);
+});
+
 
 reload()
 
